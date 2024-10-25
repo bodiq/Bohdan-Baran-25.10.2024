9dba5d41e7a0e11dfa78b983d801d84faa31662a
bodiq <bodia.baran.o@gmail.com>
2024-10-25 14:21:02 +0300

    Added Joystick and GameManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    
+    [Header("Input Setup")]
+    [SerializeField] private VariableJoystick joystick;
+    
+    
+    public VariableJoystick Joystick { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
