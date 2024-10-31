9b330553db346a8ae7cbb82ab3dba706cd2908c4
bodiq <bodia.baran.o@gmail.com>
2024-10-31 19:02:17 +0200

    Added scriptable obejct and initialization for resources images

---
+++ Assets/Scripts/Configs/ResourcesInformation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Data;
+using Enums;
+using UnityEngine;
+
+namespace Configs
+{
+    [CreateAssetMenu(fileName = "ResourcesInformation", menuName = "Resources Information")]
+    public class ResourcesInformation : ScriptableObject
+    {
+        [SerializeField] private List<ResourcesInfo> resourcesInfo;
+
+        public Dictionary<ResourceType, Sprite> resourcesInformation = new ();
+
+        private void OnEnable()
+        {
+            resourcesInformation.Clear();
+        
+            foreach (var info in resourcesInfo)
+            {
+                if (!resourcesInformation.ContainsKey(info.resourceType))
+                {
+                    resourcesInformation[info.resourceType] = info.resourceSprite;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Tiles/ResourcesIndicator.cs
+++ Assets/Scripts/Tiles/ResourcesIndicator.cs
@@ -1,3 +1,4 @@
+using Configs;
 using Enums;
 using TMPro;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class ResourcesIndicator : MonoBehaviour
     [SerializeField] private TextMeshProUGUI resourcesEarnedText;
     [SerializeField] private TextMeshProUGUI resourcesToGoText;
     [SerializeField] private Image resourceImage;
+    [SerializeField] private ResourcesInformation resourcesInformation;
 
     private int _resourcesToEarn;
     private int _resourcesEarned;
@@ -16,6 +18,11 @@ public class ResourcesIndicator : MonoBehaviour
     public void Initialize(int countToEarn, ResourceType resourceType)
     {
         _resourcesToEarn = countToEarn;
+        _resourceType = resourceType;
         resourcesToGoText.text = _resourcesToEarn.ToString();
+        if (resourcesInformation.resourcesInformation.TryGetValue(_resourceType, out var sprite))
+        {
+            resourceImage.sprite = sprite;
+        }
     }
 }
