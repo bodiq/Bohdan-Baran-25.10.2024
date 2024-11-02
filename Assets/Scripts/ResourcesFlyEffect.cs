1402641d0e1fb265a3063adde522fa3d3a425e48
bodiq <bodia.baran.o@gmail.com>
2024-11-02 11:43:51 +0200

    Added DoTween

---
+++ Assets/Scripts/ResourcesFlyEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ResourcesFlyEffect : MonoBehaviour
+    {
+        public void TriggerResourceFly(Vector3 startPosition, Vector3 endPosition, ResourcesIndicator resourcesIndicator)
+        {
+            
+        }
+    }
+}
