f79a6886a447991320ef4d997d11ace827dcb151
bodiq <bodia.baran.o@gmail.com>
2024-11-02 13:03:48 +0200

    Added some more settings and modified the visualization of resources fall

--- Assets/Scripts/ResourceItem.cs
+++ Assets/Scripts/ResourceItem.cs
@@ -6,43 +6,47 @@ namespace DefaultNamespace
 {
     public class ResourceItem : MonoBehaviour
     {
-        private Vector3 _randomPositionRange = new (0.5f, 0.5f, 0.5f);
-        private Vector3 _randomRotationRange = new (0, 360, 0);
+        private readonly Vector3 _randomSpawnPositionRange = new (0.5f, 0.5f, 0.5f);
+        private readonly Vector3 _randomSpawnRotationRange = new (0, 360, 0);
+
+        private readonly Vector3 _randomEndPositionFlyRange = new (1.2f, 0f, 1.2f);
 
         private Vector3 _randomPositionOffset;
+        private Vector3 _randomEndPositionOffset;
         
-        private float _randomXOffsetPosition;
-        private float _randomYOffsetPosition;
-        private float _randomZOffsetPosition;
-        
-        private float _randomXOffsetRotation;
-        private float _randomYOffsetRotation;
-        private float _randomZOffsetRotation;
-
         private Tween _jumpTween;
         
         private void Awake()
         {
-            _randomXOffsetPosition = Random.Range(-_randomPositionRange.x, _randomPositionRange.x);
-            _randomYOffsetPosition = Random.Range(-_randomPositionRange.y, _randomPositionRange.y);
-            _randomZOffsetPosition = Random.Range(-_randomPositionRange.z, _randomPositionRange.z);
+            var randomXOffsetPosition = Random.Range(-_randomSpawnPositionRange.x, _randomSpawnPositionRange.x);
+            var randomYOffsetPosition = Random.Range(-_randomSpawnPositionRange.y, _randomSpawnPositionRange.y);
+            var randomZOffsetPosition = Random.Range(-_randomSpawnPositionRange.z, _randomSpawnPositionRange.z);
             
-            _randomXOffsetRotation = Random.Range(0, _randomRotationRange.x);
-            _randomYOffsetRotation = Random.Range(0, _randomRotationRange.y);
-            _randomZOffsetRotation = Random.Range(0, _randomRotationRange.z);
+            var randomXOffsetEndPosition = Random.Range(-_randomEndPositionFlyRange.x, _randomEndPositionFlyRange.x);
+            var randomZOffsetEndPosition = Random.Range(-_randomEndPositionFlyRange.z, _randomEndPositionFlyRange.z);
+            
+            var randomXOffsetRotation = Random.Range(0, _randomSpawnRotationRange.x);
+            var randomYOffsetRotation = Random.Range(0, _randomSpawnRotationRange.y);
+            var randomZOffsetRotation = Random.Range(0, _randomSpawnRotationRange.z);
 
-            _randomPositionOffset = new Vector3(_randomXOffsetPosition, _randomYOffsetPosition, _randomZOffsetPosition);
-            transform.rotation = Quaternion.Euler(_randomXOffsetRotation, _randomYOffsetRotation, _randomZOffsetRotation);
+            _randomPositionOffset = new Vector3(randomXOffsetPosition, randomYOffsetPosition, randomZOffsetPosition);
+            _randomEndPositionOffset = new Vector3(randomXOffsetEndPosition, 0.7f, randomZOffsetEndPosition);
+            transform.rotation = Quaternion.Euler(randomXOffsetRotation, randomYOffsetRotation, randomZOffsetRotation);
         }
 
         public void TriggerResourceFly(Vector3 startPosition, Vector3 endPosition, ResourcesIndicator resourcesIndicator)
         {
             transform.position = startPosition + _randomPositionOffset;
+
+            var firstEndPosition = endPosition + _randomEndPositionOffset;
             
-            _jumpTween = transform.DOJump(endPosition, 2f, 1, 1f).OnComplete(() =>
+            _jumpTween = transform.DOJump(firstEndPosition, 2.5f, 1, 0.7f).SetEase(Ease.Linear).OnComplete(() =>
             {
-                ResourcePoolManager.Instance.ReturnResource(resourcesIndicator.ResourceType, this);
-                resourcesIndicator.IncreaseResourceAmount();
+                transform.DOMove(endPosition, 0.2f).OnComplete(() =>
+                {
+                    ResourcePoolManager.Instance.ReturnResource(resourcesIndicator.ResourceType, this);
+                    resourcesIndicator.IncreaseResourceAmount();
+                });
             });
         }
     }
