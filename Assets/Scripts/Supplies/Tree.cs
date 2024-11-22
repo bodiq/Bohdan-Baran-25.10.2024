0a037cc758e3afbd5cb17349711824df17fe9c45
bodiq <bodia.baran.o@gmail.com>
2024-11-22 13:08:34 +0200

    Added tree animation Wind + Spawn

--- Assets/Scripts/Supplies/Tree.cs
+++ Assets/Scripts/Supplies/Tree.cs
@@ -1,22 +1,59 @@
-﻿using DG.Tweening;
-using Managers;
+﻿
+using DG.Tweening;
+
 using UnityEngine;
 
 namespace Supplies
 {
     public class Tree : Resource
     {
+        private Tween _windEffect;
+        private Tween _rotateSpawnEffect;
+
+        private float _randomXRotation;
+        private float _randomZRotation;
+
+        private float _randomRotationDuration;
+
+        private Vector3 _endRotationVector;
+        
+        private void Start()
+        {
+            _randomXRotation = Random.Range(1f, 5f);
+            _randomZRotation = Random.Range(1f, 5f);
+
+            _randomRotationDuration = Random.Range(1.5f, 2.5f);
+
+            _endRotationVector = new Vector3(_randomXRotation, 0f, _randomZRotation);
+            StartWindEffect();
+        }
+
         protected override void RespawnResource()
         {
             LastIndexTaken = 0;
             ResetAllPieces();
             
-            RespawnTween?.Kill();
+            transform.rotation = Quaternion.identity;
             
-            RespawnTween = resourcePiecesGroupObject.transform.DOScale(Vector3.one, 0.4f).OnComplete(() =>
+            RespawnTween?.Kill();
+            _rotateSpawnEffect?.Kill();
+
+            _rotateSpawnEffect = resourcePiecesGroupObject.transform.DORotate(new Vector3(0, 360, 0), 0.6f, RotateMode.FastBeyond360);
+            
+            RespawnTween = resourcePiecesGroupObject.transform.DOScale(Vector3.one, 0.7f).SetEase(Ease.OutBack).OnComplete(() =>
             {
                 resourceCollider.enabled = true;
+                StartWindEffect();
             });
+        }
+
+        private void StartWindEffect()
+        {
+            _windEffect?.Kill();
+            transform.rotation = Quaternion.identity;
+
+            _windEffect = resourcePiecesGroupObject.transform.DORotate(_endRotationVector, _randomRotationDuration)
+                .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
         }
     }
 }
